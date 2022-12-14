87cac8832d25b912389f68d125f08e8d2ffec0dd
christina <christinayxh@163.com>
2022-12-14 23:46:16 +0800

    slide button -scroll-view

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -1,40 +1,54 @@
 
+import IconArrowLeft from '@/assets/img/svg/icon_arrow_left'
+import IconArrowRight from '@/assets/img/svg/icon_arrow_right'
 import React, { memo,useEffect,useState ,useRef} from 'react'
 import { ViewWrapper } from './style'
 
 const ScrollView = memo((props) => {
-  //定义内部的状态，showRight设置右侧按钮
-  const  [showRight,setShowRight] = useState(false)
-  const [posIndex,setPosIndex] = useState(0)//记录每次item距离左侧scroll的距离
-  const totalDistanceRef= useRef()//1.
+  const [showLeft,setShowLeft] = useState(false)//左1 
+  const  [showRight,setShowRight] = useState(false)//一 、定义内部的状态，showRight设置右侧按钮
+  const [posIndex,setPosIndex] = useState(0)//A 记录每次选中的item的index=0的元素
+  const totalDistanceRef= useRef()//1.用ref scroll边框位置获取这个元素
   //组件渲染完毕，判断是否显示右侧的按钮
   const scrollContentRef = useRef()
   useEffect(()=>{
     const scrollWidth = scrollContentRef.current.scrollWidth //可滚动的宽度
     const clientWidth = scrollContentRef.current.clientWidth //当前文档占的宽度
     const totalDistance = scrollWidth - clientWidth
-    totalDistanceRef.current = totalDistance//2.
-    setShowRight(totalDistance > 0 ) //右侧按钮显示的判断
-
+    totalDistanceRef.current = totalDistance//2.用ref来记录distance的距离
+    setShowRight(totalDistance > 0 ) //二、右侧按钮c显示的判断 
   },[props.children])//依赖children的数据
 
-  function rightClickHandle(){
-    const newIndex = posIndex + 1
-    const newEl = scrollContentRef.current.children[newIndex]//代表点击选中的元素
+  function controlClickHandle(isRight){
+    const newIndex = isRight ? posIndex + 1 : posIndex - 1 // 拿到新的偏移的item的index索引，因为数组可以用number去访问这个元素
+    const newEl = scrollContentRef.current.children[newIndex] // 通过newIndex获取新的元素
     const newOffsetLeft = newEl.offsetLeft//设置这个元素距离最左边边框offset的距离
-    scrollContentRef.current.style.transform = `translate(-${newOffsetLeft}px)`
+    scrollContentRef.current.style.transform = `translate(-${newOffsetLeft}px)`//设置滚动的距离
     setPosIndex(newIndex)
-    //是否继续显示右侧按钮
-    setShowRight(totalDistanceRef.current > newOffsetLeft)//3.
+    //是否继续显示左和右侧按钮
+    setShowRight(totalDistanceRef.current > newOffsetLeft)//3.用最新的distance做比较
+    setShowLeft(newOffsetLeft > 0 ) // 都是用newOffLeft做比较
   }
 
   return (
    <ViewWrapper>
-    <button>左边按钮</button>
-    {showRight && <button onClick={rightClickHandle}>右边按钮</button>}
-    <div className='scroll-content' ref={scrollContentRef}>
+    { showLeft && (
+      <div className = 'control left' onClick={e => controlClickHandle(false)}>
+      <IconArrowLeft/>
+      </div>
+    )}
+    { showRight && (
+      <div className = 'control right' onClick={e => controlClickHandle(true)}>
+      <IconArrowRight/>
+      </div>
+    )}
+    <div className='scroll'>
+      <div className='scroll-content' ref={scrollContentRef}>
+        {/* 使用ref设置dom来获取元素 */}
       {props.children}
+      </div>
     </div>
+   
    </ViewWrapper>
   )
 })
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -1,11 +1,42 @@
 import styled from "styled-components";
 
 export const ViewWrapper = styled.div`
-  overflow: auto;
-  .scroll-content {
-    position: relative; //设置相对定位，才能获取item本身的位置/不是body的位置
+  position: relative; //设置相对定位，才能获取item本身的位置/不是body的位置
+  padding: 8px 0;
+  .scroll {
+    overflow: auto;
+    .scroll-content {
+      display: flex;
+      transition: transform 250ms ease;
+    }
+  }
+  .control {
+    position: absolute;
+    z-index: 9;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 25px;
+    border-radius: 20%;
+    text-align: center;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #fff;
+    background: #fff;
+    box-shadow: 0px 1px 1px 1px rgba(0, 0, 0, 0.14);
+    cursor: pointer;
 
-    transition: transform 250ms ease;
+    &.left {
+      left: 0;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    &.right {
+      right: -15px;
+      top: 50%;
+      transform: translate(50%, -50%);
+    }
   }
 `;
